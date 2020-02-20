3dbfeeac4299728554661b289d45b7349936d819
Greg Meess <greg@sutromachine.com>
2020-02-20 12:55:28 -0800

    Bring in unit tests, convert to .NET Standard

---
+++ geometry3Sharp.Tests/AssertExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace g3.Tests
+{
+    public static class AssertExtensions
+    {
+        public static void AreEqual(Vector2d expected, Vector2d actual)
+        {
+            if (!expected.EpsilonEqual(actual, MathUtil.Epsilon))
+                throw new AssertFailedException($"AssertExtensions.Assert failed. Expected:{expected}. Actual{actual}");
+        }
+
+    }
+}
